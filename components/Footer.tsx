fb204eba36b030fbac2efabdf59c37efbac0587b
Muneto <punchkids3713@gmail.com>
2023-10-14 21:04:00 +0900

    add Link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
         <footer className="bg-green-700 text-white text-center">
             <div className="container max-w-7xl md:flex md:justify-start md:items-center justify-center mx-auto p-5">
                 <ul className="container flex md:justify-start justify-center mx-auto md:p-0 pb-5">
-                    <li className=""><a href="/">Home</a></li>
+                    <li className=""><Link href="/">Home</Link></li>
                     <li className="ml-4"><Link href="/#about">About</Link></li>
                     <li className="ml-4"><Link href="/#service">Service</Link></li>
                     <li className="ml-4"><Link href="/#works">Works</Link></li>
