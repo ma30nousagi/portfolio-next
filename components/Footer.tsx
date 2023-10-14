369e95494320968f4ad748c5dceb3e4e4588817c
Muneto <punchkids3713@gmail.com>
2023-10-14 21:02:53 +0900

    add Link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,11 +6,11 @@ export function Footer() {
             <div className="container max-w-7xl md:flex md:justify-start md:items-center justify-center mx-auto p-5">
                 <ul className="container flex md:justify-start justify-center mx-auto md:p-0 pb-5">
                     <li className=""><a href="/">Home</a></li>
-                    <li className="ml-4"><a href="/#about">About</a></li>
-                    <li className="ml-4"><a href="/#service">Service</a></li>
-                    <li className="ml-4"><a href="/#works">Works</a></li>
-                    <li className="ml-4"><a href="/#skills">Skills</a></li>
-                    <li className="ml-4"><a href="/#history">History</a></li>
+                    <li className="ml-4"><Link href="/#about">About</Link></li>
+                    <li className="ml-4"><Link href="/#service">Service</Link></li>
+                    <li className="ml-4"><Link href="/#works">Works</Link></li>
+                    <li className="ml-4"><Link href="/#skills">Skills</Link></li>
+                    <li className="ml-4"><Link href="/#history">History</Link></li>
                     <li className="ml-4"><Link href="https://socketweb.net/contact/">Contact</Link></li>
                 </ul>
                 <small className="p-5">&copy;2023</small>
