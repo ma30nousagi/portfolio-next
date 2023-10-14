d66f6cfd2f36c3642e82d5f91061d407bbb6b751
Muneto <punchkids3713@gmail.com>
2023-10-14 17:04:42 +0900

    remove header-link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,11 @@
-import Link from 'next/link'
-
 export function Header() {
     return (
         <header className="relative text-green-300 border">
             <div className="container max-w-7xl flex mx-auto flex-col md:flex-row items-center p-5">
                 <div className="mb-4 md:mb-0">
-                <Link href={`/`}>
-                    <a className="text-2xl font-bold">
+                    <a href="/" className="text-2xl font-bold">
                         Portfolio
                     </a>
-                </Link>
                 </div>
                 <nav className="md:ml-auto text-base">
                     <ul className="flex">
